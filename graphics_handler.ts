934e3da10691fb5385057509b9abe93ee24a1896
danielddj <daniel.djarf@gmail.com>
2023-02-15 01:05:48 +0100

    Finalized simple moving and made code readable

--- graphics_handler.ts
+++ graphics_handler.ts
@@ -1,27 +1,20 @@
 
 import { 
-    $, $$, $$$, is_null  
+    $,
+    $$, is_null  
 } from "./utilites";
 
 import { 
-    starting_pos, board_color, board   
+    board_color, board, pieces,   
 } from "./initial.config";
 
-import { 
-    pieces   
-} from "./pieces.config";
 
-import { 
-    intitial_game   
-} from "./pieces_handler";
-
-
-export function arr_to_html(current_board: any) {
-    Object.keys(current_board).forEach(val => {
-        const current_element = document.getElementById(val)
+export function arr_to_html(c_board: board): void {
+    Object.keys(c_board).forEach(val => {
+        const current_element = $(val)
         if (!is_null(current_element)) {
-            if (current_board[val].piece !== undefined) {
-                current_element.innerHTML = `${current_board[val].piece} <img class='allimg' src="${pieces[current_board[val].piece]}" alt="">`
+            if (c_board[val].piece !== null) {
+                current_element.innerHTML = `${c_board[val].piece} <img class='allimg' src="${pieces[c_board[val].piece]}" alt="">`
                 current_element.style.cursor = 'pointer' 
             } else {}  
         } else {
@@ -30,11 +23,18 @@ export function arr_to_html(current_board: any) {
     });
 }
 
+export function move_piece_graphically(to: string, from: string): void {
+    $(to).innerHTML = $(from).innerHTML
+    $(from).innerHTML = ""
+    $(from).style.cursor = "default"
+    $(to).style.cursor = "pointer"
+}
+
 export function coloring() {
-    const color = document.querySelectorAll<HTMLElement>('.box') 
+    const color = $$('.box') 
     let i: number = 0
 
-    color.forEach(square => { 
+    color.forEach((square: any) => { 
         if (board_color[i] === "w") {
             square.style.backgroundColor = 'rgb(255, 254, 254)'
             square.style.outline = "1px solid black";
