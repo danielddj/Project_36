d0970f82ad278cf5008681e3b03ce54d697f08f0
danielddj <daniel.djarf@gmail.com>
2023-03-05 22:10:49 +0100

    Changes for test branch

--- initial.config.ts
+++ initial.config.ts
@@ -145,24 +145,4 @@ export const c_board: board_type  = {
 }
 
 
-interface pieces_type {
-    [key: string]: string;
-}
-
-
-//Neccesary to display images
-export const pieces: pieces_type= {"Wpawn": require("./images/Wpawn.png"), 
-                                "Wqueen": require("./images/Wqueen.png"), 
-                                "Wrook": require("./images/Wrook.png"), 
-                                "Wknight": require("./images/Wknight.png"), 
-                                "Wking": require("./images/Wking.png"), 
-                                "Wbishop": require("./images/Wbishop.png"),
-                                "Bpawn": require("./images/Bpawn.png"), 
-                                "Bqueen": require("./images/Bqueen.png"), 
-                                "Brook": require("./images/Brook.png"), 
-                                "Bknight": require("./images/Bknight.png"), 
-                                "Bking": require("./images/Bking.png"), 
-                                "Bbishop": require("./images/Bbishop.png") }
-
-
 
